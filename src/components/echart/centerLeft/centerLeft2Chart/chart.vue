1fa80ff5b590e0791e93f6937484ab397eadbc92
Candide <biuvip@163.com>
2022-10-02 12:56:18 +0800

    first commit

---
+++ src/components/echart/centerLeft/centerLeft2Chart/chart.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <Echart id="centreLeft2Chart" ref="centreLeft2ChartRef" :options="options" height="360px" width="330px"></Echart>
+  </div>
+</template>
+
+<script>
+import Echart from '@/common/echart';
+export default {
+  data() {
+    return {
+      options: {
+  title: {
+    text: '',
+    subtext: '',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '5%',
+    left: 'center'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [
+        { value: 1048, name: 'Search Engine' },
+        { value: 735, name: 'Direct' },
+        { value: 580, name: 'Email' },
+        { value: 484, name: 'Union Ads' },
+        { value: 300, name: 'Video Ads' }
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+},
+    };
+  },
+  components: {
+    Echart,
+  },
+  props: {
+    cdata: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  watch: {
+    cdata: {
+    },
+  },
+  methods: {
+    // 开启定时器
+    startInterval() {
+      const _self = this;
+      // 应通过接口获取配置时间，暂时写死5s
+      const time = 2000;
+      if (this.intervalId !== null) {
+        clearInterval(this.intervalId);
+      }
+      this.intervalId = setInterval(() => {
+      }, time);
+    },
+  },
+};
+</script>
