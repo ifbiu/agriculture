afab57dcbccecd2c8b2570bbf4359bc85a4d5c41
Candide <biuvip@163.com>
2022-10-08 09:38:20 +0800

    icon修复

--- src/request/config.js
+++ src/request/config.js
@@ -1,6 +1,6 @@
 export const baseURL = 'https://agriculture-api.ifbiu.com' // 请求的url地址baseUrl
 // export const baseURL = 'http://localhost:8008' // 请求的url地址baseUrl
-export const weatherIconsURL = 'https://candide.oss-cn-beijing.aliyuncs.com/agriclture/icons/' // 请求的天气icons
+export const weatherIconsURL = 'https://candide.oss-cn-beijing.aliyuncs.com/agriclture/icon/' // 请求的天气icons
 
 
 export default {
