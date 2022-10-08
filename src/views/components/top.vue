08e580ec70dfb44f24afe69b28ed578be70b3fe9
Candide <biuvip@163.com>
2022-10-08 19:14:45 +0800

    生产总值

--- src/views/components/top.vue
+++ src/views/components/top.vue
@@ -5,7 +5,7 @@
       <div class="d-flex jc-center">
         <dv-decoration-8 class="dv-dec-8" :color="decorationColor" />
         <div class="title">
-          <span class="title-text">农业大数据统计展示系统</span>
+          <span class="title-text"><a href="/">农业大数据统计展示系统</a></span>
           <dv-decoration-6
               class="dv-dec-6"
               :reverse="true"
